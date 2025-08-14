6c0d4633fcadf125a536b46278a6e352fefe6c09
Sergio <sergiohrodriguess@gmail.com>
2025-08-13 21:42:15 -0300

    Generics part1

---
+++ Generics-set-dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics_set_dictionary.Entities;
+
+PrintService printService = new PrintService();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i=0; i<n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
