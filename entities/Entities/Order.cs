d092141d202170e6764037edb1bef708b2f1eac1
Sergio <sergiohrodriguess@gmail.com>
2025-09-10 06:55:22 -0300

    restriçõe para generics, criado método "Max" para receber tipos diferentes..

--- entities/Entities/Order.cs
+++ entities/Entities/Order.cs
@@ -35,6 +35,7 @@ namespace entities.Entities
             {
                 return item.SubTotal();
             }
+            return 0;
         }
     }
 }
---
+++ generics-restrictions/Program.cs
@@ -0,0 +1,27 @@
+﻿using generics_restrictions.Services;
+
+namespace generics_restrictions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+
+        Console.WriteLine("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            list.Add(x);
+        }
+        
+        CalculationService calculationService = new CalculationService();
+
+        int max = calculationService.Max(list);
+
+        Console.WriteLine("Max:");
+        Console.WriteLine(max);
+    }
+}
