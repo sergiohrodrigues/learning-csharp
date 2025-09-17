c13dcf99e9fb282a39c3e8bcb7881d0ff001e498
Sergio <sergiohrodriguess@gmail.com>
2025-09-17 06:52:29 -0300

    Exercicio proposto resolvido com uma observação... aula de Dictionary/SortedDictionary, exercicio de Dictionary/SortedDictionary resolvido

---
+++ Generics-set-dictionary/Exercicio-dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercicio_dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> set = new Dictionary<string, int>();
+        
+        Console.Write("Enter file full path: ");
+        string path = Console.ReadLine();
+        try {
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    int valor = int.Parse(line[1]);
+
+                    if (set.ContainsKey(name))
+                        set[name] += valor;
+                    else
+                        set[name] = valor;
+                }
+
+                foreach (KeyValuePair<string, int> item in set)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+        }
+        catch (IOException e) {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Generics-set-dictionary/Exercicio-proposto/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Exercicio_proposto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Eu fiz
+        // SortedSet nao adiciona valores duplicados, então perder informação se o mesmo aluno estiver em mais de um curso, conta apenas 1 vez..
+        SortedSet<int> codigosAlunos = new SortedSet<int>();
+
+        Console.Write("How many students for course A? ");
+        int studentsA = int.Parse(Console.ReadLine());
+        for (int i = 0; i < studentsA; i++)
+        {
+            codigosAlunos.Add(int.Parse(Console.ReadLine()));
+        }
+        
+        Console.Write("How many students for course B? ");
+        int studentsB = int.Parse(Console.ReadLine());
+        for (int i = 0; i < studentsB; i++)
+        {
+            codigosAlunos.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.Write("How many students for course C? ");
+        int studentsC = int.Parse(Console.ReadLine());
+        for (int i = 0; i < studentsC; i++)
+        {
+            codigosAlunos.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Total students: " + codigosAlunos.Count);
+        
+        //Professor fez
+        HashSet<int> courseA = new HashSet<int>();
+        HashSet<int> courseB = new HashSet<int>();
+        HashSet<int> courseC = new HashSet<int>();
+
+        Console.Write("How many students for course A? ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            int cod = int.Parse(Console.ReadLine());
+            courseA.Add(cod);
+        }
+
+        Console.Write("How many students for course B? ");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            int cod = int.Parse(Console.ReadLine());
+            courseB.Add(cod);
+        }
+
+        Console.Write("How many students for course C? ");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            int cod = int.Parse(Console.ReadLine());
+            courseC.Add(cod);
+        }
+
+        HashSet<int> all = new HashSet<int>(courseA);
+        all.UnionWith(courseB);
+        all.UnionWith(courseC);
+        Console.WriteLine("Total students: " + all.Count);
+    }
+}
